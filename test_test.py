1d0c1665d4ba1345bcdc70fa7d5aa37e97c9699c
darius-dinu <darius.dinu2007@gmail.com>
2022-08-05 11:03:58 +0300

    Modified while function in the PY script

--- test_test.py
+++ test_test.py
@@ -3,27 +3,27 @@
 import math
 
 
-# def functie(a, b):
-#     if a > b:
-#         return a
-#     elif a < b:
-#         return b
-#     else:
-#         return math.sqrt(a * b)
+def functie(a, b):
+    if a > b:
+        return a
+    elif a < b:
+        return b
+    else:
+        return math.sqrt(a * b)
     
-# # nebunie = functie(4, 3)
-# # print(nebunie)
-
-
-
-# def f(a, b):
-#     if a > b:
-#         return a* b
-#     else:
-#         return functie(a, b)
-
-# nebunie = f(7, 7)
+# nebunie = functie(4, 3)
 # print(nebunie)
+
+
+
+def f(a, b):
+    if a > b:
+        return a* b
+    else:
+        return functie(a, b)
+
+nebunie = f(7, 7)
+print(nebunie)
 
 
 
