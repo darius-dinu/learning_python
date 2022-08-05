a30a72f751670025b28c89bb655aff27c2f9f490
darius-dinu <darius.dinu2007@gmail.com>
2022-08-05 11:48:12 +0300

    Added two mini-games to the PY script

---
+++ calculator.py
@@ -0,0 +1,4 @@
+a = input("Introdu un numar: ")
+b = input("Introdu inca un numar: ")
+c = float(a) + float(b)
+print("Suma celor doua numere este: " + str(c))
---
+++ mad_libs.py
@@ -0,0 +1,9 @@
+print("Trece [substantiv la singular] pe ape")
+print("Printre [substantiv la plural] sa se [verb]")
+
+a = input("Introdu substantivul la singular: ")
+b = input("Introdu substantivul la plural: ")
+c = input("Introdu verbul: ")
+
+print("Trece " + a + " pe ape")
+print("Printre " + b + " sa se " + c)
