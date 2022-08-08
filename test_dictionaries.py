7ad32e3d35ffb665e791141c77d0dc2167f04380
darius-dinu <darius.dinu2007@gmail.com>
2022-08-08 15:54:23 +0300

    Added a dictionaries test file to the PY script

---
+++ test_dictionaries.py
@@ -0,0 +1,16 @@
+monthConversions = {    #Un dictionar este folosit pentru a stocka 'key value pairs' (perechi de felul a : b)
+    1 : "Ianuarie",
+    2 : "Februarie",
+    3 : "Martie",
+    4 : "April",
+    5 : "Mai",
+    6 : "Iunie",
+    7 : "Iulie",
+    8 : "August",
+    9 : "Septembrie",
+    10 : "Octombrie",
+    11 : "Noiembrie",
+    12 : "Decembrie",
+}
+
+print(monthConversions.get(1))
