348f3e252cd6d0753d155a335768fe2b99f789cb
darius-dinu <darius.dinu2007@gmail.com>
2022-08-08 15:30:28 +0300

    Added a new exercise to the PY script

--- exercises.py
+++ exercises.py
@@ -185,3 +185,18 @@ def exponent(base, exp):
         return "Your result isn't a whole number"
 
 print(exponent(2, 3))
+
+
+# 13. Write a program that returns a list that contains only the elements that are common between the lists (without duplicates). 
+# Make sure your program works on two lists of different sizes.
+
+def functie(a, b):
+    lista = []
+    for numar in a:
+        if numar in b:
+                lista.append(numar)
+    return lista
+
+f = functie([1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+print(f)
+                
