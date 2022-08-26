3280e1d8d8d5876f44b2ec4b7a1cb51cd987a206
darius-dinu <darius.dinu2007@gmail.com>
2022-08-26 16:26:56 +0300

    Added a module test file to the PY script

---
+++ test_modules.py
@@ -0,0 +1,5 @@
+import tools
+
+print(tools.suma([1, 2, 3]))                # Apeleaza functia de calculare a sumei din fisierul tools.py
+print(tools.produs([1, 2, 3, 4, 5]))        # Apeleaza functia de calculare a produsului din fisierul tools.py
+print(tools.num_generator(10))              # Apeleaza functia de generare a numerelor din fisierul tools.py  
