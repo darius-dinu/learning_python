77e3c4c3cdeb6b2aab6c0c06fb56a6e26b48aab0
darius-dinu <darius.dinu2007@gmail.com>
2022-08-26 17:05:04 +0300

    Added objects file to the PY script

---
+++ test_class.py
@@ -0,0 +1,6 @@
+from membru import Member
+
+membru1 = Member("Andrei", 15, "Buzau")
+membru2 = Member("Larisa", 20, "Chiajna")
+print(membru1.nume)             #Codul intoarce parametrul de nume al obiectului numit 'membru1'
+print(membru2.localitate)       #Codul intoarce parametrul de localitate al obiectului numit 'membru2'
