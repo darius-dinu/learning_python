4e619157642b85f3052f08d3da576bb301ce05f6
darius-dinu <darius.dinu2007@gmail.com>
2022-08-26 17:14:39 +0300

    Edited comments in the PY script

--- membru.py
+++ membru.py
@@ -2,4 +2,4 @@ class Member:       # Numele class-ului pe care il faci. Atunci cand importam ac
     def __init__(self, nume, varsta, localitate):       # Functia aceasta imi poate creea objects pe care le definesc scriind parametri doriti.
         self.nume = nume                                # Objectul nou va lua numele pe care il introduc.
         self.varsta = varsta                            # Objectul nou va lua varsta pe care o introduc.
-        self.localitate = localitate                    # Objectul noi va lua localitatea pe care o introduc.
+        self.localitate = localitate                    # Objectul nou va lua localitatea pe care o introduc.
